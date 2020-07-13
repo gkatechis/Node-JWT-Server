23127c6b63233747875aea5b3847ab367856d674
Greg <gkatechis@zendesk.com>
2020-07-13 18:31:25 -0500

    Testing router changes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const http = require('http');
+const router = express.Router();
 
 const PORT = 80;
-
 
 const handlers = require('./handlers');
 
@@ -15,16 +15,41 @@ const server = http.createServer((req,res) => {
     // console.log(req);
     res.end();
 })
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.raw());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.raw());
 app.use(cookieParser());
 app.use('/handlers', handlers)
 
-// app.post('/signin', signIn);
-// app.get('/welcome', welcome);
-// app.post('/refresh', refresh);
-// app.get('/', (req, res) => res.send('Hello World!'))
+// Sign-in handler
+router.get('/signin', (req, res) => {
+        // Get creds from JSON
+        const {
+            username,
+            password
+        } = req.body;
+        console.log(req.body);
+        if (!username || !password || users[username] !== password) {
+            //401 if user/pass aren't present or if the password is wrong
+            return res.status(401).end();
+        } else next()
+    },
+    (req, res, next) => {
+        // Create new token from usernam in payload; expires in 30s
+        const token = jwt.sign({
+            username
+        }, jwtKey, {
+            algorithm: 'HS256',
+            expiresIn: jwtExpirySeconds,
+        })
+        console.log('token: ', token);
+
+        // Set cookie as token string
+        res.cookie('token', token, {
+            maxAge: jwtExpirySeconds * 1000
+        })
+        res.render('regular');
+    });
 
 server.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`)
--- handlers.js
+++ handlers.js
@@ -1,8 +1,4 @@
 const jwt = require('jsonwebtoken');
-const express = require('express');
-const router = express.Router();
-
-
 const jwtKey = 'E871C21F-F765-40F3-A0CE-ECF366B7B375|BD71A8D1-40F0-49CC-AA21-B6B956DF48B7';
 const jwtExpirySeconds = 300;
 
@@ -10,55 +6,43 @@ const users = {
     user1: 'password1',
     user2: 'password2',
 };
-
-// Sign-in handler
-const signIn = router.post('/signin', (req,res) => {
-    // Get creds from JSON
-    const {username, password} = req.body;
-    console.log(req.body);
-    if (!username || !password || users[username] !== password) {
-        //401 if user/pass aren't present or if the password is wrong
-        return res.status(401).end();
-    };
-
-    // Create new token from usernam in payload; expires in 30s
-    const token = jwt.sign({username}, jwtKey, {
-        algorithm: 'HS256',
-        expiresIn: jwtExpirySeconds,
-    })
-    console.log('token: ', token);
-
-    // Set cookie as token string
-    res.cookie('token', token, {
-        maxAge: jwtExpirySeconds * 1000
-    })
-    res.end();
+// No mount path, always returns
+router.use(function (req, res, next) {
+    console.log('Time:', Date.now());
+    next();
 });
 
-// Welcome handler
+const signIn = router.use('/signin', (req, res, next) => {
+    console.log('Request URL:', req/originalURL);
+    next()
+},
+(req, res, next) => {
+    console.log('Request Type:', req.method);
+});
 
-const welcome = (req,res) => {
-    // Get token from request's cookies
-    const token = req.cookies.token
+// // Welcome handler
+// router.get('/welcome = (req,res) => {
+//     // Get token from request's cookies
+//     const token = req.cookies.token
 
-    // No cookie? 401 for you.
-    if (!token) {
-        return res.status(401).end();
-    };
+//     // No cookie? 401 for you.
+//     if (!token) {
+//         return res.status(401).end();
+//     };
 
-var payload
-try {
-    payload = jwt.verify(token, jwtKey);
-} catch (e) {
-    if (e instanceof jwt.JsonWebTokenError) {
-        return res.status(401).end();
-    }
-    return res.status(400).end();
-};
+// var payload
+// try {
+//     payload = jwt.verify(token, jwtKey);
+// } catch (e) {
+//     if (e instanceof jwt.JsonWebTokenError) {
+//         return res.status(401).end();
+//     }
+//     return res.status(400).end();
+// };
 
-res.send(`Welcome ${payload.username}!`)
+// res.send(`Welcome ${payload.username}!`)
 
-};
+// };
 
 // Refresh handler since expiry is short
 
