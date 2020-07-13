ab47b424e467f5e67989d0384d218c1a1f8d241c
Greg <gkatechis@zendesk.com>
2020-07-13 13:34:43 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const PORT = 8000;
+
+const {signIn, welcome, refresh} = require('./handlers');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/signin', signIn);
+app.get('/welcome', welcome);
+app.post('/refresh', refresh);
+
