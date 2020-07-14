611b4c2f184665695a6d0a5ada12bd8fdc470036
Greg <gkatechis@zendesk.com>
2020-07-14 17:20:53 -0500

    Removing error handling for the time being

--- app.js
+++ app.js
@@ -28,26 +28,26 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/signin', signIn);
-// app.get('/welcome', welcome);
-// app.post('/refresh', refresh);
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.post('/signin', signIn);
+// // app.get('/welcome', welcome);
+// // app.post('/refresh', refresh);
+// app.get('/', (req, res) => res.send('Hello World!'))
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//     next(createError(404));
+// });
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function (err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
 
 app.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`)
